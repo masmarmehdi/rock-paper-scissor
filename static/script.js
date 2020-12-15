8e7cf6648a909b05db0a45e9b8c3fcbb020347bc
Mehdi Masmar <mmasmar@c1r10p11.khpi.ucode-connect.study>
2020-12-15 17:25:46 +0200

    update

--- static/script.js
+++ static/script.js
@@ -9,7 +9,7 @@ function rpsGame(yourChoice){
 }
 function botRandomChoice(){
     return Math.floor(Math.random()*3);
-}
+} 
 function numberToChoice(number){
     return ['rock','paper','scissor'][number];
 }
@@ -43,23 +43,25 @@ function rpsFrontEnd(yourImageChoice,botImageChoice,finalMessage){
     document.getElementById('rock').remove();
     document.getElementById('paper').remove();
     document.getElementById('scissor').remove();
+    //creating an elements after removing items
     var yourDiv=document.createElement('div');
     var botDiv=document.createElement('div');
     var messageDiv=document.createElement('div');
     var resultShown=document.createElement('div');
     var resultShown1=document.createElement('div');
+    //add an inner HTML to the elements that i have created
     resultShown.innerHTML="<h2 style=background-color:#002b80;color:white;padding:10px;>You:</h2>";
     resultShown1.innerHTML="<h2 style=background-color:#002b80;color:white;padding:10px;>:Computer</h2>";
     yourDiv.innerHTML="<img src='"+imageDataBase[yourImageChoice]+"' width=150 height=150;>";
     botDiv.innerHTML="<img src='"+imageDataBase[botImageChoice]+"' width=150 height=150;>";
     messageDiv.innerHTML="<h1 style=color:"+finalMessage['color']+";font-size:40px;background-color:"+finalMessage['background-color']+";>"+finalMessage['message']+"</h1>";
+    //appending my elements to the users
     document.getElementById('flex-box-rps-div').appendChild(resultShown);
     document.getElementById('flex-box-rps-div').appendChild(yourDiv);
     document.getElementById('flex-box-rps-div').appendChild(messageDiv);
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
     document.getElementById('flex-box-rps-div').appendChild(resultShown1);
 }
-
 function playAgain(){
     location.reload(); 
 }
